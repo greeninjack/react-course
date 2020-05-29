bed0c3daecd7c5cb6116d86861ecd8beb26583e4
greeninjack <greeninjack@gmail.com>
2020-05-29 19:16:28 +0300

    Burger Menu just after transforming a Object into a Array

--- burger-menu-no-eject/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-menu-no-eject/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,24 @@
 import React,{ Component } from "react";
 import Aux from '../../hoc/Aux'
+import Burger from '../../components/Burger/Burger'
+
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+        salad: 2,
+        bacon: 1,
+        cheese: 2,
+        meat: 2,
+        }
+    }
     render() {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
