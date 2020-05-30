e4023ba97c72df90d7b03325e329a64b65eb6172
greeninjack <greeninjack@gmail.com>
2020-05-30 17:54:41 +0300

    middle of burger menu S8L134

--- burger-menu-no-eject/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-menu-no-eject/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,15 @@
 import React,{ Component } from "react";
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+//you typically name constants you want to use as global constants in all capital characters.
+const INGREDIENT_PRICES = {
+    SALAD: 0.5,
+    CHEESE: 0.4,
+    MEAT: 1.3,
+    BACON: 0.7,
+}
 
 class BurgerBuilder extends Component {
     // constructor(props) {
@@ -9,17 +18,58 @@ class BurgerBuilder extends Component {
     // }
     state = {
         ingredients: {
-        salad: 2,
-        bacon: 1,
-        cheese: 2,
-        meat: 2,
-        }
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0,
+        },
+        totalPrice: 4,
     }
+
+addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updateCount = oldCount + 1;
+    const updatedIngredients = {
+        ...this.state.ingredients
+    }
+    updatedIngredients[type] = updateCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    
+};
+removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if(oldCount <= 0) {
+        return;
+    }
+    const updateCount = oldCount - 1;
+    const updatedIngredients = {
+        ...this.state.ingredients
+    }
+    updatedIngredients[type] = updateCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+};
+
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls 
+                ingredientAdded={this.addIngredientHandler}
+                ingredientRemoved={this.removeIngredientHandler}
+                disabled={disabledInfo}
+                />
             </Aux>
         );
     }
